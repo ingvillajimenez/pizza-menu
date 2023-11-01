98c5f396977aac39dd3908641d8aaa870358ab68
Juan Villa <ing.villa.jimenez@gmail.com>
2023-11-01 11:11:09 -0600

    project done

--- src/index.js
+++ src/index.js
@@ -139,7 +139,7 @@ function Pizza({ pizzaObj }) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 8;
+  const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
